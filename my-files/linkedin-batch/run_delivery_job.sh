6e38953f4b61c149d99865f8f930b22ab079f8fa
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2024-05-28 17:50:05 -0300

    Batch status control - FAILED Status

--- my-files/linkedin-batch/run_delivery_job.sh
+++ my-files/linkedin-batch/run_delivery_job.sh
@@ -1,7 +1,7 @@
 CURRENT_DATE=`date '+%Y/%m/%d'`
 LESSON=$(basename $PWD)
 #UUID=`uuidgen`
-UUID=9
+UUID=10
 mvn clean package -Dmaven.test.skip=true;
 java -jar ./target/linkedin-batch-0.0.1-SNAPSHOT.jar "item=shoes" "run.date(date)=$CURRENT_DATE" "lesson=$LESSON" "uuid=$UUID";
 read;
