9110cf2b157450863dba589acb975f585512e4f1
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2024-05-28 13:24:25 -0300

    Configure a job repository

--- README.md
+++ README.md
@@ -4,3 +4,38 @@
 
 [Spring Batch Page](https://spring.io/projects/spring-batch)  
 [Spring Batch Doc](https://docs.spring.io/spring-batch/reference/index.html)  
+[MySQL Docker Image](https://hub.docker.com/_/mysql)  
+
+## Commands
+
+```
+Create Docker network:
+docker network create my-bridge-network
+
+Start a MySQL instance:
+docker run --name some-mysql --network my-bridge-network -e MYSQL_ROOT_PASSWORD=batch -d mysql:latest
+
+List networks:
+docker network ls
+
+Show containers and its networks:
+docker ps --format '{{ .ID }} {{ .Names }} {{ json .Networks }}'
+
+Inspect a network and shows the containers associated with it:
+docker network inspect my-bridge-network
+
+See logs:
+docker logs some-mysql
+
+Get the ip address of a container:
+docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' some-mysql
+
+Connect to MySQL (using IP):
+docker run -it --network my-bridge-network --rm mysql mysql -h172.21.0.2 -uroot -p
+
+SQL queries used:
+create database batch_repo;
+select * from BATCH_JOB_EXECUTION\G
+select * from BATCH_JOB_INSTANCE\G
+select * from BATCH_STEP_EXECUTION\G
+```
---
+++ my-files/linkedin-batch-02-02-end/src/main/java/com/linkedin/batch/LinkedinBatchApplication.java
@@ -0,0 +1,50 @@
+package com.linkedin.batch;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableBatchProcessing
+public class LinkedinBatchApplication {
+	
+	@Autowired
+	public JobBuilderFactory jobBuilderFactory;
+	
+	@Autowired
+	public StepBuilderFactory stepBuilderFactory;
+	
+	@Bean
+	public Step packageItemStep() {
+		return this.stepBuilderFactory.get("packageItemStep").tasklet(new Tasklet() {
+			
+			@Override
+			public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+				System.out.println("The item has been packaged.");
+				return RepeatStatus.FINISHED;
+			}
+		}).build(); 
+	}
+	
+	@Bean
+	public Job deliverPackageJob() {
+		return this.jobBuilderFactory.get("deliverPackageJob").start(packageItemStep()).build();
+	}
+
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkedinBatchApplication.class, args);
+	}
+
+}
