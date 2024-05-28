175292f7452521928a32d45687f1dc17ac85ae5a
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2024-05-28 17:44:37 -0300

    Batch status control - STOPPED Status

--- my-files/linkedin-batch/src/main/java/com/linkedin/batch/LinkedinBatchApplication.java
+++ my-files/linkedin-batch/src/main/java/com/linkedin/batch/LinkedinBatchApplication.java
@@ -97,7 +97,7 @@ public class LinkedinBatchApplication {
 	@Bean
 	public Step driveToAddressStep() {
 		
-		boolean GOT_LOST = false;
+		boolean GOT_LOST = true;
 		return this.stepBuilderFactory.get("driveToAddressStep").tasklet(new Tasklet() {
 			
 			@Override
@@ -133,7 +133,8 @@ public class LinkedinBatchApplication {
 		return this.jobBuilderFactory.get("deliverPackageJob")
 				.start(packageItemStep())
 				.next(driveToAddressStep())
-					.on("FAILED").to(storePackageStep())
+					//.on("FAILED").to(storePackageStep())
+				.on("FAILED").stop()
 				.from(driveToAddressStep())
 					.on("*").to(decider())
 						.on("PRESENT").to(givePackageToCustomerStep())
