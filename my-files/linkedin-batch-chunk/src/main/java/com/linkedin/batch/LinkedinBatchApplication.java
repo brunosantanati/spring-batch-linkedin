9306482d5ca10bc973a1127f181bbd78a8b84090
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2024-05-29 19:10:55 -0300

    Configure chunk-oriented step

---
+++ my-files/linkedin-batch-chunk/src/main/java/com/linkedin/batch/LinkedinBatchApplication.java
@@ -0,0 +1,60 @@
+package com.linkedin.batch;
+
+import java.util.List;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableBatchProcessing
+public class LinkedinBatchApplication {
+	
+	@Autowired
+	public JobBuilderFactory jobBuilderFactory;
+	
+	@Autowired
+	public StepBuilderFactory stepBuilderFactory;
+
+	@Bean
+	public ItemReader<String> itemReader() {
+		return new SimpleItemReader(); 
+	}
+	
+	@Bean
+	public Step chunkBasedStep() {
+		return this.stepBuilderFactory.get("chunkBasedStep")
+				.<String,String>chunk(3)
+				.reader(itemReader())
+				.writer(new ItemWriter<String>() {
+
+					@Override
+					public void write(List<? extends String> items) throws Exception {
+						System.out.println(String.format("Received list of size: %s", items.size()));
+						items.forEach(System.out::println);
+					}
+					
+				}).build();
+	}
+	
+
+	@Bean
+	public Job job() {
+		return this.jobBuilderFactory.get("job")
+				.start(chunkBasedStep())
+				.build();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkedinBatchApplication.class, args);
+	}
+
+}
