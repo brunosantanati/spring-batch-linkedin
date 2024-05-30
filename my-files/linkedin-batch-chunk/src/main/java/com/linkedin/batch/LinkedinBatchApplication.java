11657b192094b742c1974a74953400646f035992
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2024-05-29 22:03:42 -0300

    Read flat file (CSV)

--- my-files/linkedin-batch-chunk/src/main/java/com/linkedin/batch/LinkedinBatchApplication.java
+++ my-files/linkedin-batch-chunk/src/main/java/com/linkedin/batch/LinkedinBatchApplication.java
@@ -9,15 +9,21 @@ import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.FileSystemResource;
 
 @SpringBootApplication
 @EnableBatchProcessing
 public class LinkedinBatchApplication {
 	
+	public static String[] tokens = new String[] {"order_id", "first_name", "last_name", "email", "cost", "item_id", "item_name", "ship_date"};
+
 	@Autowired
 	public JobBuilderFactory jobBuilderFactory;
 	
@@ -25,19 +31,33 @@ public class LinkedinBatchApplication {
 	public StepBuilderFactory stepBuilderFactory;
 
 	@Bean
-	public ItemReader<String> itemReader() {
-		return new SimpleItemReader(); 
+	public ItemReader<Order> itemReader() {
+		FlatFileItemReader<Order> itemReader = new FlatFileItemReader<Order>();
+		itemReader.setLinesToSkip(1);
+		itemReader.setResource(new FileSystemResource("/home/bruno/data/shipped_orders.csv"));
+		
+		DefaultLineMapper<Order> lineMapper = new DefaultLineMapper<Order>();
+		DelimitedLineTokenizer tokenizer = new DelimitedLineTokenizer();
+		tokenizer.setNames(tokens);
+		
+		lineMapper.setLineTokenizer(tokenizer);
+		
+		lineMapper.setFieldSetMapper(new OrderFieldSetMapper());
+		
+		itemReader.setLineMapper(lineMapper);
+		return itemReader;
+		
 	}
 	
 	@Bean
 	public Step chunkBasedStep() {
 		return this.stepBuilderFactory.get("chunkBasedStep")
-				.<String,String>chunk(3)
+				.<Order,Order>chunk(3)
 				.reader(itemReader())
-				.writer(new ItemWriter<String>() {
+				.writer(new ItemWriter<Order>() {
 
 					@Override
-					public void write(List<? extends String> items) throws Exception {
+					public void write(List<? extends Order> items) throws Exception {
 						System.out.println(String.format("Received list of size: %s", items.size()));
 						items.forEach(System.out::println);
 					}
